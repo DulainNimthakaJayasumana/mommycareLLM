28b3061494f8d6f378a25a52a91fd39291122ee7
DulainNimthakaJayasumana <59439269+DulainNimthakaJayasumana@users.noreply.github.com>
2025-04-08 23:56:01 +0530

    final working version

--- api.py
+++ api.py
@@ -61,9 +61,6 @@ async def get_answer_voice(audio_file: UploadFile = File(...)):
             except Exception:
                 pass
 
-# The following block will not be used by Vercel's serverless functions,
-# so it can remain for local development.
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
